97dac6a2f391194bd2c3fd557dc351786951bece
vbelkina <72238100+vbelkina@users.noreply.github.com>
2021-09-30 17:59:41 -0400

    CPA1

--- App.js
+++ App.js
@@ -1,21 +1,106 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+import { StyleSheet, Text, View, Button, Image} from 'react-native';
+
+import Plants from './components/Plants';
+import About from './components/About';
+
+const Stack = createNativeStackNavigator();
+
+const MyStack = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer>
+      <Stack.Navigator>
+
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          //options={{ title: 'Welcome' }}
+        />
+
+        <Stack.Screen name="About" component={About} />
+        <Stack.Screen name="Plants" component={Plants} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style = {{backgroundColor: 'lavender', flex:1}}>
+      <View style={{ flexDirection: 'row',
+                     margin:"25px",
+                     border:"solid black",
+                     padding:'10px',
+                     justifyContent: 'space-around', }}>
+        <Button
+          title="About"
+          color='green'
+          onPress={() =>
+            navigation.navigate('About')
+          }
+        />  
+
+        <Button
+          title="Plants"
+          color='green'
+          onPress={() =>
+            navigation.navigate('Plants')
+          }
+        />
+      </View>
+
+      <View style = {{justifyContent: 'center', alignItems: 'center'}}>
+          <Text> Home Screen. There will be stuff about plants and whatnot here</Text>
+          <View style={{flexDirection:'row'}}>
+          <Image
+              style={styles.forImages}
+              source={{
+                uri: 'https://cdn.britannica.com/20/211920-050-8E3E17C5/flowering-dogwood-tree.jpg',
+              }}
+            />
+          <Image
+              style={styles.forImages}
+              source={{
+                uri: 'https://bloximages.newyork1.vip.townnews.com/greensboro.com/content/tncms/assets/v3/editorial/8/77/8776e160-e286-11eb-ba69-83c2c4f6cbf5/60eb54631e955.image.jpg?resize=1200%2C800',
+              }}
+            />
+            <Image
+              style={styles.forImages}
+              source={{
+                uri: 'https://www.calloways.com/wp-content/uploads/fiddle-leaf-fig-tree-form-0773402761.jpg',
+              }}
+            />
+            <Image
+              style={styles.forImages}
+              source={{
+                uri: 'https://hips.hearstapps.com/hbu.h-cdn.co/assets/15/30/1437681387-daisy-fun-facts.jpg',
+              }}
+            />
+            <Image
+              style={styles.forImages}
+              source={{
+                uri: 'https://i.pinimg.com/originals/0e/2e/c8/0e2ec83000543de204474e5e7e9ff4b1.jpg',
+              }}
+            />
+          </View>
+      </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  forImages: {
+    width: 300,
+    height: 300,
+    borderColor: 'black',
+    borderWidth: 1,
   },
 });
+
+export default MyStack;
+
---
+++ components/Plants.js
@@ -0,0 +1,103 @@
+import * as React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+function Plants() {
+  return (
+    <View style={{flex:1, backgroundColor: 'lavender'}}>
+      <View style={styles.container}>
+        <View style={styles.para} >
+          <Text style={{fontSize:50}}>PLANT #1</Text>
+          <Text>This is the plants page. We will be able to store information about plants we have at home here</Text>
+          <Text>This is some more text about plants</Text>
+          <Text>This is some more text about plants</Text>
+          <Text>This is some more text about plants</Text>
+          <Text>This is some more text about plants</Text>
+        </View>
+        <View style={{flex: 1}}>
+        <Image
+            style={styles.forImages}
+            source={{
+              uri: 'https://www.calloways.com/wp-content/uploads/fiddle-leaf-fig-tree-form-0773402761.jpg',
+            }}
+          />
+        </View>
+        <View style={styles.para} >
+            <Text style={{fontSize:50}}>PLANT #2</Text>
+            <Text>This is the plants page. We will be able to store information about plants we have at home here</Text>
+            <Text>This is some more text about plants</Text>
+            <Text>This is some more text about plants</Text>
+            <Text>This is some more text about plants</Text>
+            <Text>This is some more text about plants</Text>
+        </View>
+        <View style={{flex: 1}}>
+          <Image
+              style={styles.forImages}
+              source={{
+                uri: 'https://www.thespruce.com/thmb/B4D_VovqxAJErFAMOy2KXWY1KqY=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/peperomia-plant-4584414-10-e05d776cfe614311932bc4d25f0bd768.jpg',
+              }}
+            />
+        </View>
+      </View>
+      <View style={styles.container}>
+        <View style={styles.para} >
+          <Text style={{fontSize:50}}>PLANT #3</Text>
+          <Text>This is the plants page. We will be able to store information about plants we have at home here</Text>
+          <Text>This is some more text about plants</Text>
+          <Text>This is some more text about plants</Text>
+          <Text>This is some more text about plants</Text>
+          <Text>This is some more text about plants</Text>
+        </View>
+        <View style={{flex: 1}}>
+        <Image
+            style={styles.forImages}
+            source={{
+              uri: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F37%2F2021%2F03%2F18%2FGrow-citrus-indoors-102107770.jpg',
+            }}
+          />
+        </View>
+        <View style={styles.para} >
+            <Text style={{fontSize:50}}>PLANT #4</Text>
+            <Text>This is the plants page. We will be able to store information about plants we have at home here</Text>
+            <Text>This is some more text about plants</Text>
+            <Text>This is some more text about plants</Text>
+            <Text>This is some more text about plants</Text>
+            <Text>This is some more text about plants</Text>
+        </View>
+        <View style={{flex: 1}}>
+          <Image
+              style={styles.forImages}
+              source={{
+                uri: 'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_1:1/at%2Fhome-projects%2F2021-03%2Fshutterstock_1119774236',
+              }}
+            />
+        </View>
+      </View>
+    </View> 
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex:1,
+    flexDirection: 'row',
+    padding: 20,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  forImages: {
+    width: 300,
+    height: 300,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+  para: {
+    flex: 1,
+    alignItems: 'baseline',
+    justifyContent: 'space-evenly'
+
+  }
+});
+
+export default Plants;
